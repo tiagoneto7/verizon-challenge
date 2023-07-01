0bf3cfe945fdebdc57503c2b5084a39ba592fa04
Tiago Neto <tineto@sapo.pt>
2023-07-02 00:56:17 +0100

    setup

---
+++ script.js
@@ -0,0 +1,19 @@
+const handleMenu = () => {
+  let element = document.getElementById("burguer-menu");
+  if (element.style.display === "block") {
+    element.style.display = "none";
+  } else {
+    element.style.display = "block";
+  }
+};
+
+const validateNif = () => {
+  let value = document.getElementById("nif-input").value;
+  let error = document.getElementById("nif-error");
+
+  if (value.length > 8) {
+    error.style.display = "none";
+  } else {
+    error.style.display = "block";
+  }
+};
